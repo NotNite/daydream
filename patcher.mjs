cc59216afdb37a30c24521950d2634a95614f3b2
NotNite <hi@notnite.com>
2024-12-10 16:33:52 -0500

    Copy only when needed

--- patcher.mjs
+++ patcher.mjs
@@ -9,19 +9,6 @@ const exists = async (filepath) =>
     .then(() => true)
     .catch(() => false);
 
-// Patches web-side stuff
-async function patch(filePath) {
-  let orig = await fs.readFile(filePath, "utf8");
-  let source = orig;
-
-  if (filePath.includes("index-")) {
-    source = source.replaceAll(`"Low Priority"`, `"greets"`);
-  }
-
-  if (orig !== source) await fs.writeFile(filePath, source);
-}
-
-// This is really slow, we should ideally mark when something's been patched to not copy it again
 async function copyDir(src, dest) {
   if (!(await exists(dest))) {
     await fs.mkdir(dest, { recursive: true });
@@ -33,13 +20,6 @@ async function copyDir(src, dest) {
     const data = await fs.stat(srcFile);
     if (data.isFile()) {
       await fs.copyFile(srcFile, destFile);
-      if (destFile.endsWith(".js") || destFile.endsWith(".mjs")) {
-        try {
-          await patch(destFile);
-        } catch (e) {
-          console.error(e);
-        }
-      }
     } else {
       if (!(await exists(destFile))) {
         await fs.mkdir(destFile, { recursive: true });
@@ -50,21 +30,74 @@ async function copyDir(src, dest) {
   }
 }
 
+async function getFileTree(src) {
+  let tree = [];
+
+  for (const file of await fs.readdir(src)) {
+    const fullPath = path.join(src, file);
+    const data = await fs.stat(fullPath);
+    if (data.isFile()) {
+      tree.push(fullPath);
+    } else {
+      tree = tree.concat(await getFileTree(fullPath));
+    }
+  }
+
+  return tree;
+}
+
 export async function inject(appInjector) {
   const hooks = path.resolve(import.meta.filename, "../hooks.mjs");
   register("file:" + hooks, import.meta.url);
 
   const appDir = path.resolve(appInjector, "..");
+  const version = JSON.parse(
+    await fs.readFile(path.join(appDir, "package.json"))
+  ).version;
   const realMain = path.join(appDir, "build/main/index.mjs");
-  const work = getWorkDir();
 
   // Copy everything into its own dir to patch it on-FS
+  const work = getWorkDir();
   if (!(await exists(work))) {
     await fs.mkdir(work, { recursive: true });
   }
-  for (const dir of ["build", "build-browser"]) {
-    await copyDir(path.join(appDir, dir), path.join(work, dir));
+
+  const cacheFile = path.join(work, ".ver");
+  let cachedVersion = (await exists(cacheFile))
+    ? await fs.readFile(cacheFile, "utf8")
+    : null;
+
+  if (version != cachedVersion) {
+    for (const dir of ["build", "build-browser"]) {
+      await copyDir(path.join(appDir, dir), path.join(work, dir));
+    }
+    await fs.writeFile(cacheFile, version);
   }
+
+  const tree = (await getFileTree(work)).map((p) =>
+    path.relative(work, p).replaceAll("\\", "/")
+  );
+  async function modify(pathRegex, cb) {
+    for (const entry of tree) {
+      if (pathRegex.test(entry)) {
+        try {
+          const source = await fs.readFile(path.join(appDir, entry), "utf8");
+          const modified = await cb(source);
+          if (source !== modified) {
+            await fs.writeFile(path.join(work, entry), modified);
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    }
+  }
+
+  await modify(/build\/renderer\/index-.+?\.js/, (src) => {
+    src = src.replaceAll(`"Low Priority"`, `"greets 2"`);
+
+    return src;
+  });
 
   runMain(realMain);
 }
